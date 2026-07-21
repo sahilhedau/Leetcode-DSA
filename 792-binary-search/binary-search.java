93be40586f2942ab1b6df6b3c90254526291662b
sahilhedau <sahilsandeephedau@gmail.com>
2026-07-21 21:04:45 +0530

    Time: 1 ms (10.11%) | Memory: 47.7 MB (97.73%) - LeetSync

---
+++ 792-binary-search/binary-search.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left <= right){
+            if(nums[left]==target){
+                return left;
+            }
+            left++;
+        }
+    return -1;
+    }
+}
